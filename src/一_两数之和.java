216af2bd47843962d8788297057e723761e21655
lujianxiong <1143936981@qq.com>
2021-07-26 22:12:17 +0800

    更新一下另一台电脑的笔记

--- src/一_两数之和.java
+++ src/一_两数之和.java
@@ -1,24 +1,31 @@
 import java.util.HashMap;
-import java.util.Map;
 
 /**题目：
- *     给定一个整数数组nums和一个整数目标值target，请你在该数组中找出和为目标值target的那两个整数，并返回他们的数组下标
+ *  给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个整数，并返回它们的数组下标。
+ （你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。）
  */
 
 public class 一_两数之和 {
-    public static void main(String[] args) {
-        int[] a = {99,22,12,56,32,88,102,2,6,7,0};
-        int[] results = twoSum2(a, 120);
-        for(int i=0; i<results.length; i++){
-            System.out.print(results[i]+" ");
+    public static void main(String args[]){
+        int[] a = {1,2,3,4,5,6,7,8,9};
+        int[] result = sum2(a,3);
+        for(int i=0; i<result.length;i++){
+            System.out.print(result[i]+",");
         }
 
     }
 
-    public static int[] twoSum1(int[] nums, int target){
+    /**
+     * 在一个数组中寻找两数之和为target的数,并返回他们的下标
+     * @param nums 在该数组中寻找两数
+     * @param target  要寻找的两数和的目标值
+     * @return
+     */
+    //1、暴力枚举法
+    public static int[] sum1(int nums[], int target){
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] == target){
+                if (nums[i] + nums[j] == target){
                     return new int[]{i,j};
                 }
             }
@@ -26,16 +33,18 @@ public class 一_两数之和 {
         return new int[0];
     }
 
-    public static int[] twoSum2(int[] nums, int target){
-        Map<Integer, Integer> map = new HashMap<>();  //map的key存的是数组的值，map的value存的是数组的索引
+    //2、哈希表
+    public static int[] sum2(int nums[], int target){
+        HashMap<Integer, Integer> map = new HashMap<>();  //key存的是索引处的值，value存的是索引
+
         for(int i=0; i<nums.length; i++){
-            if(map.containsKey(target - nums[i])){   //如果map中存在键和当前索引i处的值相加为target
-                return new int[]{map.get(target - nums[i]),i};  //那么返回：map的键target-nums[i]的value和索引i
+            if (map.containsKey( target - nums[i]) ){
+                return new int[]{i,map.get(target - nums[i])};
             }
-            map.put(nums[i],i);  //如果map中没有满足条件的，则将当前索引处的元素存入map中
+            map.put(nums[i], i);
         }
+
         return new int[0];
     }
-
 
 }
--- src/二_两数之加.java
+++ src/二_两数之加.java
@@ -1,13 +1,12 @@
 import java.util.LinkedList;
 import java.util.List;
 
-public class 二_两数之加 {
+/**题目：
+ *  给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个整数，并返回它们的数组下标。
+ （你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。）
+ */
 
-    /**题目：
-     *     给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只存储一位数字。
-     *     请你将两个数相加，并以相同的形式返回一个表示他们和的链表。
-     *     （你可以假设除了数字0之外，这两个数都不会以0开头）
-     */
+public class 二_两数之加 {
 
     public static void main(String args[]){
         LinkedList l1 = new LinkedList();
@@ -57,5 +56,38 @@ public class 二_两数之加 {
             }
         }
         return temp;
+    }
+
+
+
+    public static Node add(Node n1, Node n2){
+        //构造一个空的链表
+        Node head = null;
+        Node tail = null;
+
+        return null;
+
+    }
+
+
+    //定义链表的节点
+    class Node{
+        //成员变量（属性）
+        int val;
+        Node next;
+
+        //成员方法
+        //无参构造
+        public Node(){
+        }
+
+        //有参构造
+        public Node(int val){
+            this.val = val;
+        }
+        public Node(int val, Node next){
+            this.val = val;
+            this.next = next;
+        }
     }
 }
