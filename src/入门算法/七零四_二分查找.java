0d38bcd906729834ca12b22915ed6a5e462fe359
lujianxiong <1143936981@qq.com>
2021-07-27 15:00:21 +0800

    先从入门级算法开始刷叭

---
+++ src/入门算法/七零四_二分查找.java
@@ -0,0 +1,18 @@
+package 入门算法;
+
+/**
+ * 题目：
+ *      给定一个n个元素有序的(升序)整型数组nums和一个目标值target，写一个函数搜索nums中的target，如果目标值存在返回下标，否则返回-1
+ */
+
+public class 七零四_二分查找 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int search(int[] nums, int target){
+
+        return -1;
+    }
+
+}
